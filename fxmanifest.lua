14f791561fcfeaaed8ebf444ff8c43c56b04e9ac
Walterr399 <walterwhiterp47@gmail.com>
2025-02-26 15:46:16 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+author "Walter399"
+description "Greenzone"
+version "1.0"
+
+client_scripts {"client/*.lua"}
+shared_scripts {"@ox_lib/init.lua", "config.lua"}
