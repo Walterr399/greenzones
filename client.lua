78603e0986dc814bd57be4f4d1f181a60c6378d5
Walterr399 <walterwhiterp47@gmail.com>
2025-04-08 14:23:04 +0000

    Updated Client & Config

--- client.lua
+++ client.lua
@@ -1,63 +1,73 @@
-Citizen.CreateThread(function()
-    local inZone = false
-    local currentZone = nil
+-- // [VARIABLES] \\ --
+local inGreenZone = false
+local nearestZone = nil
 
+-- // [THREADS] \\ --
+CreateThread(function()
     while true do
+        local sleep = 1000
         local ped = PlayerPedId()
         local pos = GetEntityCoords(ped)
-        local zoneFound = false
-        local zoneNameFound = nil
+        local inZone = false
+        local currentZone
 
-        for zoneName, zoneData in pairs(Config.Greenzone) do
-            if #(pos - zoneData.coords) < zoneData.radius then
-                zoneFound = true
-                zoneNameFound = zoneName
+        for _, zone in pairs(Config.GreenZones) do
+            local distance = #(pos - zone.coords)
+            if distance <= zone.radius then
+                inZone = true
+                currentZone = zone
                 break
             end
         end
 
-        if zoneFound then
-            if not inZone then
-                inZone = true
-                exports.ox_lib:notify({
-                    title = "Greenzone - Status",
-                    description = "You have entered a green zone.",
-                    position = "top",
-                    type = "inform"
-                })
-            end
-
-            SetEntityInvincible(ped, true)
-            SetPlayerInvincible(PlayerId(), true)
-
-            local maxHealth = GetEntityMaxHealth(ped)
-            if GetEntityHealth(ped) < maxHealth then
-                SetEntityHealth(ped, maxHealth)
-            end
-
-            if GetSelectedPedWeapon(ped) ~= GetHashKey("WEAPON_UNARMED") then
-                SetCurrentPedWeapon(ped, GetHashKey("WEAPON_UNARMED"), true)
-            end
-
-            DisableControlAction(0, 24, true)
-            DisableControlAction(0, 25, true)
-            DisableControlAction(0, 37, true)
-        else
-            if inZone then
-                inZone = false
-                currentZone = nil
-                exports.ox_lib:notify({
-                    title = "Greenzone - Status",
-                    description = "You have left the green zone.",
-                    position = "top",
-                    type = "inform"
-                })
-            end
-
-            SetEntityInvincible(ped, false)
-            SetPlayerInvincible(PlayerId(), false)
+        if inZone and not inGreenZone then
+            inGreenZone = true
+            nearestZone = currentZone
+            enterZone(currentZone)
+        elseif not inZone and inGreenZone then
+            inGreenZone = false
+            exitZone(nearestZone)
+            nearestZone = nil
         end
 
-        Citizen.Wait(zoneFound and 0 or 500)
+        Wait(sleep)
     end
 end)
+
+CreateThread(function()
+    while true do
+        if inGreenZone then
+            DisablePlayerFiring(PlayerId(), true)
+            DisableControlAction(0, 24, true) 
+            DisableControlAction(0, 25, true) 
+            DisableControlAction(0, 140, true) 
+            DisableControlAction(0, 141, true)
+            DisableControlAction(0, 142, true)
+            DisableControlAction(0, 143, true)
+            DisableControlAction(0, 37, true) 
+            DisableControlAction(0, 44, true) 
+            DisableControlAction(0, 289, true) 
+            SetCurrentPedWeapon(PlayerPedId(), `WEAPON_UNARMED`, true)
+            Wait(0)
+        else
+            Wait(500)
+        end
+    end
+end)
+
+-- // [FUNCTIONS] \\ --
+function enterZone(zone)
+    lib.notify({
+        title = 'Greenzone',
+        description = ('You entered %s'):format(zone.label),
+        type = 'inform'
+    })
+end
+
+function exitZone(zone)
+    lib.notify({
+        title = 'Greenzone',
+        description = ('You left %s'):format(zone.label),
+        type = 'warning'
+    })
+end
--- config.lua
+++ config.lua
@@ -1,45 +1,9 @@
---- @class Zone
---- @field coords vector3 The coordinates of the greenzone
---- @field radius number The radius of the greenzone
-
---- @class ConfigTable
 Config = {}
 
---- @class Greenzone
---- @field Blokkenpark Zone
---- @field Ziekenhuis Zone
---- @field Cardealer Zone
---- @field spawn Zone
---- @field Casino Zone
---- @field Comserve Zone
---- @field Wapendealer Zone
-Config.Greenzone = {
-    Blokkenpark = {
-        coords = vector3(130.8985, -1065.4132, 29.1924),
-        radius = 75.0
-    },
-    Ziekenhuis = {
-        coords = vector3(310.1053, -589.3291, 90.6143),
-        radius = 120.0
-    },
-    Cardealer = {
-        coords = vector3(-29.9753, -1097.5773, 27.2743),
-        radius = 40.0
-    },
-    spawn = {
-        coords = vector3(-269.7202, -957.2246, 31.2228),
-        radius = 50.0
-    },
-    Casino = {
-        coords = vector3(-1828.3601, -1192.9412, 14.3077),
-        radius = 50.0
-    },
-    Comserve = {
-        coords = vector3(-1271.2694, -3388.4224, 12.9329),
-        radius = 100.0
-    },
-    Wapendealer = {
-        coords = vector3(5067.0186, -4591.5083, -1.1195),
+Config.GreenZones = {
+    {
+        label = "Politie Hoofdbureau",
+        coords = vec3(-581.4120, -922.7491, 19.0221),
         radius = 50.0
     }
 }
